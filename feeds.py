432b9b3881951c5a1033266eb8adee946676761b
Noam <noamoss@gmail.com>
2016-05-21 17:00:47 +0300

    complete kikar addition

--- feeds.py
+++ feeds.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import operator
 import urllib
-from datetime import datetime
+import datetime
 from time import mktime
 from urllib.parse import urlparse
-
+from flask import flash, url_for, redirect,session
+from bs4 import BeautifulSoup
 import feedparser
-from flask import session
 
+from _config import relevant_days_for_feed
 from app.models import Feed
 from db import db
 
@@ -18,15 +19,16 @@ def relevant_feeds(user_id):
 
 
 def parse_feeds(feeds):
-    # parse and mix opentaba feeds
+    # parse and mix feeds
     results = []
 
     for feed in feeds:
         feed_data = feedparser.parse(feed.url)
         for entry in feed_data.entries:
-            results.append([feed.name, entry.title, entry.summary, urllib.parse.quote(entry.link),
-                            datetime.fromtimestamp(
-                                mktime(entry.updated_parsed)).date()])
+            published_date = datetime.datetime.fromtimestamp(mktime(entry.updated_parsed)).date()
+            summary = BeautifulSoup(entry.summary).get_text()
+            if datetime.datetime.today().date() - published_date <= datetime.timedelta(relevant_days_for_feed):
+                results.append([feed.name, entry.title, summary, urllib.parse.quote(entry.link),published_date])
     return sorted(results, key=operator.itemgetter(4), reverse=True)
 
 
@@ -34,10 +36,10 @@ def set_title_by_feed(url):
     # return tuple of porject name and specific title for new feeds
     project_name = get_project_by_feed_url(url)
     try:
-        moreinfo = feedparser.parse(url).feed.title
+        title = feedparser.parse(url).feed.title
     except:
-        moreinfo=None
-    return (project_name,moreinfo)
+        title=None
+    return (project_name,title)
 
 def relevant_feeds(user_id=None):
     if user_id == None:
@@ -58,5 +60,30 @@ def get_project_by_feed_url(url):
     domain_first_part = urllib.parse.urlparse(url).netloc.split(".")[0]
     if "opentaba" in domain_first_part:
         return 'תב"ע פתוחה'
+    elif domain_first_part == "kikar":
+        return 'כיכר המדינה'
     else:
         return "לא ידוע"
+
+
+def save_feed_to_db(url, name, project,user_id,relevant_feeds):
+    if url not in relevant_feeds:
+        title = set_title_by_feed(url)
+        try:
+            title = title[1].split(" ")
+        except:
+            pass
+        a_new_feed = Feed(
+            url=url,
+            name=name,
+            user_id=user_id,
+            project=project
+        )
+        db.session.add(a_new_feed)
+        db.session.commit()
+        flash(u'ההזנה החדשה נוספה למאגר')
+        return redirect(url_for('notifier.feeds_editor'))
+    else:
+        flash(u'את/ה כבר עוקבים אחרי מקור מידע זה')
+        return redirect(url_for('notifier.feeds_editor'))
+
