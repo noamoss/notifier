87484b5f92b4d1fdca0caf3068d073baf5e6376e
Noam <noamoss@gmail.com>
2016-05-25 20:28:16 +0300

    fix wrong import

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
 
-from flask import Flask, Blueprint, render_template
+from flask import Flask, render_template
+from flask_wtf.csrf  import CsrfProtect
+from sendgrid import Mail
 
 from _config import DATABASE_PATH, BITLY_KEY,BITLY_USER
 from app.models import User
 from db import db
 from views import notifier
 
-email_sender = Blueprint('email_sender', __name__,
-                         template_folder='templates',
-                         static_folder='notifier.static_folder')
-
+csrf = CsrfProtect()
 
 def create_app():
     app = Flask(__name__)
@@ -19,20 +18,20 @@ def create_app():
     app.debug=True
     db.init_app(app)
     app.register_blueprint(notifier, url_prefix='')
-    app.register_blueprint(email_sender)
+    csrf.init_app(app)
     return app
 
 def create_database(app):
     with app.app_context():
         db.create_all()
-    user = User()
-    user.email = "noamoss@gmail.com"
-    user.password = "12345"
     db.session.commit()
-
 
 app = create_app()
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html')
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
