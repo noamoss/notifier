ace415beb0935eb3ad84db4bb7e49b6913923e5c
Noam <noamoss@gmail.com>
2016-05-27 00:17:47 +0300

    automatically redirect logged in users from login page to feed

--- views.py
+++ views.py
@@ -56,7 +56,11 @@ def login():
                 error = 'כתובת דוא"ל או סיסמה שגויים'
         else:
             error = "שני השדות דרושים להתחברות."
-    return render_template("login.html", form=form, error=error)
+    elif session['logged_in'] == True:  #if already logged in:
+        flash(u'ברוכים השבים, את/ה כבר מחוברת!')
+        return redirect(url_for('notifier.feeds_editor'))
+    else:
+        return render_template("login.html", form=form, error=error)
 
 
 @notifier.route('/logout/')
