279863bdd743c256929118ac01880eedfe095a9c
Noam <noamoss@gmail.com>
2016-06-06 21:15:43 +0300

    fix errorhandlers (404,500) templates and add 502 errorhandler

--- views.py
+++ views.py
@@ -314,3 +314,17 @@ def confirmed_email(token):
         db.session.commit()
         flash("אישרת את חשבונך, תודה!", "הצלחה")
     return redirect(url_for('notifier.login'))
+
+@notifier.app_errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
+@notifier.app_errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+@notifier.app_errorhandler(502)
+def feed_error(error):
+    return render_template('502.html'), 502
